e60e4159f779e5cdb803294275cc38a7b695f350
Shiv  Yadav <shivyadav0014@gmail.com>
2026-05-08 21:41:12 +0545

    add the route for user for giving review on courses

--- backend/src/routes/user/reviewroute.js
+++ backend/src/routes/user/reviewroute.js
@@ -1,12 +1,17 @@
+const {
+  getmyreviews,
+  deletereview,
+  createreview,
+} = require("../../controllers/user/review/reviewcontroller");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const restrict = require("../../middlewares/restrict");
+const catchasync = require("../../services/catchasync");
+const router = require("express").Router();
 
+router.route("/reviews").get(isAuthenticated, catchasync(getmyreviews));
+router
+  .route("/reviews/:id")
+  .delete(isAuthenticated, catchasync(deletereview))
+  .post(isAuthenticated, restrict("customer"), catchasync(createreview));
 
-const { getmyreviews, deletereview, createreview } = require("../../controllers/user/review/reviewcontroller")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const restrict = require("../../middlewares/restrict")
-const catchasync = require("../../services/catchasync")
-const router = require("express").Router()
-
-router.route("/reviews").get(isAuthenticated,catchasync(getmyreviews))
-router.route("/reviews/:id").delete(isAuthenticated,catchasync(deletereview)).post(isAuthenticated,restrict("customer"),catchasync(createreview))
-
-module.exports = router
+module.exports = router;
