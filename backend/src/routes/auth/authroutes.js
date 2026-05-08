8c31e6e4c305fcd2bb3b88627fdcb050ed4d9b31
Shiv  Yadav <shivyadav0014@gmail.com>
2026-05-08 21:38:07 +0545

    add the auth route

--- backend/src/routes/auth/authroutes.js
+++ backend/src/routes/auth/authroutes.js
@@ -1,19 +1,24 @@
-const { registeruser, loginuser, updateProfile, forgotpassword, verifyotp, resetpassword } = require("../../controllers/auth/authController")
+const {
+  registeruser,
+  loginuser,
+  updateProfile,
+  forgotpassword,
+  verifyotp,
+  resetpassword,
+} = require("../../controllers/auth/authController");
 
-const catchasync= require("../../services/catchasync")
-const {multer,storage} = require("../../middlewares/multerconfig")
-const upload = multer({storage:storage})
-const isAuthenticated = require("../../middlewares/authMiddleware")
+const catchasync = require("../../services/catchasync");
+const {multer, storage} = require("../../middlewares/multerconfig");
+const upload = multer({storage: storage});
+const isAuthenticated = require("../../middlewares/authMiddleware");
 
+const router = require("express").Router();
 
-const router = require("express").Router()
+router.route("/register").post(upload.single("profileimage"), catchasync(registeruser));
+router.route("/login").post(catchasync(loginuser));
+router.route("/profile").put(isAuthenticated, catchasync(updateProfile));
+router.route("/forgotpassword").post(catchasync(forgotpassword));
+router.route("/verifyotp").post(catchasync(verifyotp));
+router.route("/resetpassword").post(catchasync(resetpassword));
 
-
-router.route("/register").post(upload.single("profileimage"),catchasync( registeruser))
-router.route("/login").post(catchasync(loginuser))
-router.route("/profile").put(isAuthenticated, catchasync(updateProfile))
-router.route("/forgotpassword").post(catchasync(forgotpassword))
-router.route("/verifyotp").post(catchasync(verifyotp))
-router.route("/resetpassword").post(catchasync(resetpassword))
-
-module.exports = router
+module.exports = router;
